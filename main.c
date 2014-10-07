73d39d4738fcb9f289e87f3e006e1c30726eb408
Emeraude <thibaut.broggi.42@gmail.com>
2014-10-07 20:35:00 +0200

    remove a useless ternary condition

--- main.c
+++ main.c
@@ -19,20 +19,19 @@ static inline int	sopipa(const char **av)
   int			outfd;
   pid_t			pid;
 
-  return (((((infd = open(FILE1, O_RDONLY)) == -1
-	     || (outfd = creat(FILE2, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1
-	     || dup2(infd, 0) == -1
-	     || pipe(pipefd) == -1
-	     || (pid = fork()) == -1))
-	   || ((pid == 0)
-	       && ((close(pipefd[0]) == -1
-		    || dup2(pipefd[1], 1) == -1
-		    || execlp("/bin/sh", "/bin/sh", "-c", CMD1, (void *)0) == -1)))
-	   || ((close(pipefd[1]) == -1
-		|| dup2(pipefd[0], 0) == -1
-		|| dup2(outfd, 1) == -1
-		|| execlp("/bin/sh", "/bin/sh", "-c", CMD2, (void *)0) == -1))) ?
-	  (FAILURE) : (SUCCESS));
+  return ((((infd = open(FILE1, O_RDONLY)) == -1
+	    || (outfd = creat(FILE2, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1
+	    || dup2(infd, 0) == -1
+	    || pipe(pipefd) == -1
+	    || (pid = fork()) == -1))
+	  || ((pid == 0)
+	      && ((close(pipefd[0]) == -1
+		   || dup2(pipefd[1], 1) == -1
+		   || execlp("/bin/sh", "/bin/sh", "-c", CMD1, (void *)0) == -1)))
+	  || ((close(pipefd[1]) == -1
+	       || dup2(pipefd[0], 0) == -1
+	       || dup2(outfd, 1) == -1
+	       || execlp("/bin/sh", "/bin/sh", "-c", CMD2, (void *)0) == -1)));
 }
 
 int			main(const int ac, const char **av)
